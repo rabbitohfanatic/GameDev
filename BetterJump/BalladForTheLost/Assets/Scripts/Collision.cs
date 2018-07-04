842eca04149912f47abcf25ef58ca27d59753aee
J-Mac <j.carroll1095@gmail.com>
2018-07-04 17:06:47 +1000

    July18 Update

---
+++ BetterJump/BalladForTheLost/Assets/Scripts/Collision.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collision : MonoBehaviour {
+
+	public NewPlayerMovement movement;
+
+	private void OnCollisionEnter(UnityEngine.Collision collision)
+	{
+		if(collision.collider.tag == "Obstacle"){
+			Debug.Log("Hit Obstacle");
+
+			movement.enabled = false;
+		}
+
+
+	}
+
+}
---
+++ BetterJump/BalladForTheLost/Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour {
+
+    public Transform player;
+    public Vector3 offset;
+
+    public float speedH = 2.0f;
+    public float speedV = 2.0f;
+
+    private float yaw = 0.0f;
+    private float pitch = 0.0f;
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = player.position + offset;
+
+        yaw += speedH * Input.GetAxis("Mouse X");
+        pitch -= speedV * Input.GetAxis("Mouse Y");
+
+        transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);
+	}
+}
